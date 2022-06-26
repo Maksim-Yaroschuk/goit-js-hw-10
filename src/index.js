b229444b0de92cba550ec155b4de8ba63135fcec
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-26 19:57:36 +0300

    worked task #1

--- src/index.js
+++ src/index.js
@@ -1,70 +1,77 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-// let name = ""
 const inputCountry = document.querySelector("#search-box")
 const infoCountry = document.querySelector(".country-info")
 const listCountry = document.querySelector(".country-list")
-inputCountry.addEventListener("input", debounce(onGetCountry, 1300))
-
+inputCountry.addEventListener("input", debounce(onGetCountry, DEBOUNCE_DELAY))
+let markup = ""
 
 function onGetCountry(event) {
     console.log(event.target.value)
-    // name = event.target.value
 
-    fetchCountries(event.target.value)
-        .then((countries) => {
-            countries.map(({flags, name, capital, population, languages}) => { 
-                // console.log(country)
-                // console.log(flags.svg)
-                console.log(name.official)
-                // console.log(capital)
-                // console.log(population)
-                // console.log(languages)
-                const markup = 
-                    `<li><img width="33.8px", heigth="21.1px" src="${flags.svg}"></li>
-                     <li>${name.official}</li>`
-                listCountry.insertAdjacentHTML("afterbegin", markup) 
-                infoCountry.innerHTML = `
-                    <ul>
-                        <li><img width="101.44px", heigth="63.4px" src="${flags.svg}"> ${name.official}</li>
-                        <li>Capital: ${capital}</li>
-                        <li>Population: ${population}</li>
-                        <li>Languages: ${languages}</li>
-                    </ul> `
-            })
-                
-        })
-        .catch((error) => console.log(error));
+  if (event.target.value === "") {
+    clearField()
+    return
+  }
+  
+  fetchCountries(event.target.value)
+    .then((countries) => {
+      console.log(countries.length)
+      if (countries.length === 1) {
+        clearField()
+        renderInfoCountry(countries)
+      } else {
+        if (countries.length > 10) {
+          clearField()
+          Notify.info("Too many matches found. Please enter a more specific name.")
+          return
+        }
+        clearField()
+        renderListCountry(countries)
+      }       
+    })
+    .catch((error) => {
+      console.log(error)
+      Notify.failure("Oops, there is no country with that name")
+    });
 }
 
-// fetchCountries(name)
+function renderInfoCountry(countries) {
+  countries.map(({ flags, name, capital, population, languages }) => { 
+  markup = `<h1><img width="50", heigth="25" src="${flags.svg}"> ${name.official}</h1>
+            <p><b>Capital:</b> ${capital}</p>
+            <p><b>Population:</b> ${population}</p>
+            <p><b>Languages:</b> ${(Object.values(languages)).map(languages => languages).join(", ")}</p>`
+  infoCountry.innerHTML = markup
+  }).join("")
+}
+
+function renderListCountry(countries) {
+  
+  countries.map(({ flags, name }) => {
+  listCountry.style = `list-style-type: none`
+  markup = `<li><img width="30", heigth="15" src="${flags.svg}"> ${name.official}</li>`
+  listCountry.insertAdjacentHTML('afterbegin', markup)
+  }).join("")
+}
+
+function clearField() {
+    listCountry.innerHTML = ""
+    infoCountry.innerHTML = ""
+}
 
 function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=flags,name,capital,population,languages`).then(
     (response) => {
       if (!response.ok) {
         throw new Error(response.status);
+        
       }
       return response.json();
     }
   );
 }
-
-
-// function fetchUsers() {
-//   return fetch("https://jsonplaceholder.typicode.com/users").then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// }
-
-
-
-// console.log(inputCountry.value)
